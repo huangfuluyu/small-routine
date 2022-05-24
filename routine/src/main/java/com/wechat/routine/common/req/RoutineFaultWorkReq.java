b566edf26a8eacc0232efdf4f72b138fbeb5b227
huangfuluyu <1737437902@qq.com>
2022-05-24 23:56:11 +0800

    :art: 更改接口

--- routine/src/main/java/com/wechat/routine/common/req/RoutineFaultWorkReq.java
+++ routine/src/main/java/com/wechat/routine/common/req/RoutineFaultWorkReq.java
@@ -1,6 +1,59 @@
 package com.wechat.routine.common.req;
 
 
-public class RoutineFaultWorkReq {
+import lombok.Data;
 
+import java.util.Date;
+
+@Data
+public class RoutineFaultWorkReq {
+    /**
+     * 用户单位
+     */
+    public String userUnit;
+
+    /**
+     * 用户名
+     */
+    public String userName;
+
+    /**
+     * 性别
+     */
+    public String userGender;
+
+    /**
+     * 电话
+     */
+    public String userPhone;
+
+    /**
+     * 故障描述
+     */
+    public String faultInfo;
+
+    /**
+     * 故障图片
+     */
+    public String faultImg;
+
+    /**
+     * 工单创建时间
+     */
+    public Date workTime;
+
+    /**
+     * 地址
+     */
+    public String faultAddress;
+
+    /**
+     * 详细地址
+     */
+    public String faultDetailAddress;
+
+    /**
+     * 备注
+     */
+    public String remarks;
 }
--- routine/src/main/java/com/wechat/routine/controller/RoutineController.java
+++ routine/src/main/java/com/wechat/routine/controller/RoutineController.java
@@ -1,93 +1,100 @@
 package com.wechat.routine.controller;
 
+import com.wechat.routine.common.req.RoutineFaultWorkReq;
+import com.wechat.routine.pojo.RoutineFaultWork;
+import com.wechat.routine.pojo.RoutineMaintainer;
+import com.wechat.routine.utils.ApiResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RoutineController {
 
     /**
      * 用户登陆
      */
-    public void loginUser(){
-
+    public ApiResponse loginUser(@RequestBody RoutineFaultWorkReq req){
+        return ApiResponse.ofSuccess(null);
     }
 
     /**
      * 添加报障信息
      */
-    public void addfailure(){
-
+    public ApiResponse addFailure(@RequestBody RoutineFaultWorkReq req){
+        return ApiResponse.ofMessage(null);
     }
 
 
     /**
      * 根据手机号查询报障记录
      */
-    public void getUserFailureRecord(){
-
+    public ApiResponse getUserFailureRecord(@RequestParam Integer phone){
+        return ApiResponse.ofMessage(null);
     }
 
     /**
      * 获取工单处理进度
      */
-    public void getWorkSchedule(){
-
+    public ApiResponse getWorkSchedule(@RequestParam String workId){
+        return ApiResponse.ofMessage(null);
     }
 
     /**'
      * 查看报障信息
      */
-    public void getUserFailureInfo(){
-
+    public ApiResponse getUserFailureInfo(@RequestParam String workId){
+        return ApiResponse.ofMessage(null);
     }
 
     /**
      * 修改报障信息
      */
-    public void updateUserFailureInfo(){
-
+    public ApiResponse updateUserFailureInfo(@RequestBody RoutineFaultWork faultWork){
+        return ApiResponse.ofMessage(null);
     }
 
     /**
      * 获取维护人员信息
      */
-    public void getMaintainerInfo(){
-
+    public ApiResponse getMaintainerInfo(@RequestParam String maintainer){
+        return ApiResponse.ofMessage(null);
     }
 
     /**
      * 根据类型查询所有工单
      */
-    public void getAllFaultWorkByType(){
-
+    public ApiResponse getAllFaultWorkByType(@RequestParam String type){
+        return ApiResponse.ofMessage(null);
     }
 
     /**
      * 获取维护人员列表
      */
-    public void getMaintainer(){
-
+    public ApiResponse getMaintainer(){
+        return null;
     }
 
     /**
      * 获取该人员消息通知
      */
-    public void getUserNotice(){
-
+    public ApiResponse getUserNotice(){
+        return null;
     }
 
     /**
      * 维护人员登陆
      */
-    public void loginMaintainer(){
-
+    public ApiResponse loginMaintainer(){
+        return null;
     }
 
     /**
      * 满意度问卷
      */
-    public void addQUIS(){
-
+    public ApiResponse addQuis(){
+        return null;
     }
 
 
